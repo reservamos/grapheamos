53eaa4915ce586a979b4f9d52ed15ac205a85da6
adriancuadros <adrian@reservamos.mx>
2019-11-14 16:24:18 -0600

    Improves startup messages

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/reservamos/graphql-start/internal/graphql"
 )
 
 func main() {
 	fmt.Println("~~~~ Powered by Reservamos ~~~~")
+	fmt.Printf("Server running at: http://localhost:%s\n", os.Getenv("PORT"))
 	graphql.Start()
 }
